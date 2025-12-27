d036966fa6799ce4c10b8a6e7781e1259b62bbef
SofiaSeniv-Uni <90135240+SofiaSeniv-Uni@users.noreply.github.com>
2025-12-27 09:17:51 +0200

    Add applying reason to details pages

--- src/pages/UserPages/UserApplicationDetailsPage.jsx
+++ src/pages/UserPages/UserApplicationDetailsPage.jsx
@@ -70,15 +70,22 @@ const UserApplicationDetailsPage = ({ reservationId, onBack }) => {
                         <Typography variant="body1" sx={{ color: '#001f3f', mb: 0.5 }}>
                             Кімната № {applicationData.roomNumber}, місце № {applicationData.bedNumber}
                         </Typography>
-                        <Typography variant="body1" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
+                        <Typography variant="h6" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
                             {applicationData.phoneNumber}
+                        </Typography>
+
+                        <Typography variant="body1" sx={{ color: '#001f3f', mb: 0.5 }}>
+                            Причина бронювання
+                        </Typography>
+                        <Typography variant="h6" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
+                            {applicationData.reason}
                         </Typography>
 
                         {/* Дата поселення */}
                         <Typography variant="body1" sx={{ color: '#001f3f', mb: 0.5 }}>
                             Дата поселення
                         </Typography>
-                        <Typography variant="h5" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
+                        <Typography variant="h6" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
                             {formatDate(applicationData.reservationStartDate)}
                         </Typography>
 
@@ -86,7 +93,7 @@ const UserApplicationDetailsPage = ({ reservationId, onBack }) => {
                         <Typography variant="body1" sx={{ color: '#001f3f', mb: 0.5 }}>
                             Дата виселення
                         </Typography>
-                        <Typography variant="h5" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
+                        <Typography variant="h6" fontWeight="bold" sx={{ color: '#001f3f', mb: 2 }}>
                             {formatDate(applicationData.reservationEndDate)}
                         </Typography>
 
